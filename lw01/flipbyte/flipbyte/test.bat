df96f7c62d7b4fb9c251381fbb85afe1aa81b464
matjukov-nikolaj <blatnoi.kolyan@yandex.ru>
2018-02-21 11:08:40 +0300

    файд вывода хранится в temp

--- lw01/flipbyte/flipbyte/test.bat
+++ lw01/flipbyte/flipbyte/test.bat
@@ -5,39 +5,39 @@ REM start of the program without parameters
 IF NOT ERRORLEVEL 1 GOTO err
 
 REM start of the program with incorrect parameter
-%PROGRAM% 123abc > out.txt
+%PROGRAM% 123abc > "%TEMP%\out.txt"
 IF NOT ERRORLEVEL 1 GOTO err
 
 REM start of the program with negative parameter
-%PROGRAM% -2 > out.txt
+%PROGRAM% -2 > "%TEMP%\out.txt"
 IF NOT  ERRORLEVEL 1 GOTO err
 
 REM start of the program with very large number
-%PROGRAM% 123214312515316564224561145741635784151 > out.txt
+%PROGRAM% 123214312515316564224561145741635784151 > "%TEMP%\out.txt"
 IF NOT ERRORLEVEL 1 GOTO err
 
 REM 6 -> 96
-%PROGRAM% 6 > out.txt
+%PROGRAM% 6 > "%TEMP%\out.txt"
 IF ERRORLEVEL 1 GOTO err
-FC out.txt test1.txt
+FC "%TEMP%\out.txt" test1.txt
 IF ERRORLEVEL 1 GOTO err
 
 REM 12 -> 48
-%PROGRAM% 12 > out.txt
+%PROGRAM% 12 > "%TEMP%\out.txt"
 IF ERRORLEVEL 1 GOTO err
-FC out.txt test2.txt
+FC "%TEMP%\out.txt" test2.txt
 IF ERRORLEVEL 1 GOTO err
 
 REM 231 -> 231
-%PROGRAM% 231 > out.txt
+%PROGRAM% 231 > "%TEMP%\out.txt"
 IF ERRORLEVEL 1 GOTO err
-FC out.txt test3.txt
+FC "%TEMP%\out.txt" test3.txt
 IF ERRORLEVEL 1 GOTO err
 
 REM 249 -> 159
-%PROGRAM% 249 > out.txt
+%PROGRAM% 249 > "%TEMP%\out.txt"
 IF ERRORLEVEL 1 GOTO err
-FC out.txt test4.txt
+FC "%TEMP%\out.txt" test4.txt
 IF ERRORLEVEL 1 GOTO err
 
 
--- lw01/invert/invert/test.bat
+++ lw01/invert/invert/test.bat
@@ -9,21 +9,21 @@ REM in4.txt det == 0
 IF  NOT ERRORLEVEL 1 GOTO err
 
 REM in1.txt -> out.txt
-%PROGRAM% in1.txt > out.txt
+%PROGRAM% in1.txt > "%TEMP%\out.txt"
 IF ERRORLEVEL 1 GOTO err
-FC out.txt out1.txt
+FC "%TEMP%\out.txt" out1.txt
 IF ERRORLEVEL 1 GOTO err
 
 REM in2.txt -> out.txt
-%PROGRAM% in2.txt > out.txt
+%PROGRAM% in2.txt > "%TEMP%\out.txt"
 IF ERRORLEVEL 1 GOTO err
-FC out.txt out2.txt
+FC "%TEMP%\out.txt" out2.txt
 IF ERRORLEVEL 1 GOTO err
 
 REM in3.txt -> out.txt
-%PROGRAM% in3.txt > out.txt
+%PROGRAM% in3.txt > "%TEMP%\out.txt"
 IF ERRORLEVEL 1 GOTO err
-FC out.txt out3.txt
+FC "%TEMP%\out.txt" out3.txt
 IF ERRORLEVEL 1 GOTO err
 
 ECHO Program testing succeeded :-)
