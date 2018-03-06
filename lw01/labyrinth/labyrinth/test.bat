e7e848e12540a6f67d1413ee747b9a36f87f4f3c
matjukov-nikolaj <blatnoi.kolyan@yandex.ru>
2018-03-07 00:33:25 +0300

    добавил задание - лабиринт

--- lw01/labyrinth/labyrinth/test.bat
+++ lw01/labyrinth/labyrinth/test.bat
@@ -4,27 +4,61 @@ REM start of the program without parameters
 %PROGRAM%
 IF NOT ERRORLEVEL 1 GOTO err
 
-REM in4.txt det == 0
-%PROGRAM% in4.txt
-IF  NOT ERRORLEVEL 1 GOTO err
+REM one parameter
+%PROGRAM% in1.txt
+IF NOT ERRORLEVEL 1 GOTO err
 
-REM in1.txt -> out.txt
-%PROGRAM% in1.txt > out.txt
+REM labyrinth 1
+%PROGRAM% in1.txt "%TEMP%\out.txt"
 IF ERRORLEVEL 1 GOTO err
-FC out.txt out1.txt
-IF ERRORLEVEL 1 GOTO err
-
-REM in2.txt -> out.txt
-%PROGRAM% in2.txt > out.txt
-IF ERRORLEVEL 1 GOTO err
-FC out.txt out2.txt
+FC "%TEMP%\out.txt" out1.txt
 IF ERRORLEVEL 1 GOTO err
 
-REM in3.txt -> out.txt
-%PROGRAM% in3.txt > out.txt
+REM labyrinth 2
+%PROGRAM% in2.txt "%TEMP%\out.txt"
 IF ERRORLEVEL 1 GOTO err
-FC out.txt out3.txt
+FC "%TEMP%\out.txt" out2.txt
 IF ERRORLEVEL 1 GOTO err
+
+REM no path
+%PROGRAM% in3.txt "%TEMP%\out.txt"
+IF NOT ERRORLEVEL 1 GOTO err
+FC "%TEMP%\out.txt" out3.txt
+IF ERRORLEVEL 1 GOTO err
+
+REM no start point
+%PROGRAM% in4.txt "%TEMP%\out.txt"
+IF NOT ERRORLEVEL 1 GOTO err
+FC "%TEMP%\out.txt" out4.txt
+IF ERRORLEVEL 1 GOTO err
+
+REM a lot of finish points
+%PROGRAM% in5.txt "%TEMP%\out.txt"
+IF NOT ERRORLEVEL 1 GOTO err
+FC "%TEMP%\out.txt" out5.txt
+IF ERRORLEVEL 1 GOTO err
+
+
+REM incorrect symbol
+%PROGRAM% in6.txt "%TEMP%\out.txt"
+IF NOT  ERRORLEVEL 1 GOTO err
+FC "%TEMP%\out.txt" out6.txt
+IF ERRORLEVEL 1 GOTO err
+
+
+REM big labyrinth
+%PROGRAM% in7.txt "%TEMP%\out.txt"
+IF ERRORLEVEL 1 GOTO err
+FC "%TEMP%\out.txt" out7.txt
+IF ERRORLEVEL 1 GOTO err
+
+
+REM labyrinth size larger than 100
+%PROGRAM% in8.txt "%TEMP%\out.txt"
+IF NOT ERRORLEVEL 1 GOTO err
+FC "%TEMP%\out.txt" out8.txt
+IF ERRORLEVEL 1 GOTO err
+
 
 ECHO Program testing succeeded :-)
 
